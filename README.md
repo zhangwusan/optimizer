08546f9a61121c19dc91914b976a6f8887176adc
Ougi <ougi@192.168.0.207>
2024-12-03 19:17:53 +0700

    Add: first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# OPTIMIZE OS
+
+## Cleaning Libraries
+It has been built for the purpose of cleaning the system cache files and logs files
+
+- Clear system and application cache.
+- Remove log files.
+- Empty trash or recycle bin.
+- Find and list large files
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="optimizer",
+    version="0.1.0",
+    description="Cross-platform optimizer system cleaner library",
+    author="zhang wusan",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
